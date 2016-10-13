b630b06f55674aa13bc775bee1dfb91bf11ab86c
wangbei1101 <wangbeimk@163.com>
2016-10-13 18:35:20 +0800

    eureka-server

---
+++ ecureka-server/src/main/java/com/weber/springcloud/SpringcloudApplication.java
@@ -0,0 +1,12 @@
+package com.weber.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+}
